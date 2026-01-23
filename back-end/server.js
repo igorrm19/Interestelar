d3ccfafb2ce4316023b05827fb19d755decef758
igorrm23 <igormachadofire14@gmail.com>
2026-01-23 05:03:30 -0300

    finalizando configurações basicas

--- back-end/server.js
+++ back-end/server.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+
+    res.json({
+        message: "Hello World"
+    });
 });
 
 app.listen(3000, () => {
