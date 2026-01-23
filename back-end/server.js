2b702f4f447baefe95209b4a677ab9b4ab27ba5b
igorrm23 <igormachadofire14@gmail.com>
2026-01-23 20:37:58 -0300

    servindo index pelo server

--- back-end/server.js
+++ back-end/server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const errMiddleware = require("./middleware/err.middleware");
 const log = require("./middleware/log.middleware");
 const Router = require("./routes/arquichive.routes");
+const path = require("path");
 const app = express();
 
 app.use(express.json());
 app.use(log);
 app.use("/", Router);
+app.use(express.static(path.join(__dirname, "public")));
 app.use(errMiddleware);
 
 
