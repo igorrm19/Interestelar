7f3867a9d2d93d8eb992e7c7c4bbc1477d2aadff
igorrm23 <igormachadofire14@gmail.com>
2026-01-23 21:44:06 -0300

    corrigindo fetch

--- back-end/public/script.js
+++ back-end/public/script.js
@@ -1,13 +1,25 @@
+/**
+ * "id": "101",
+    "name": "Xenomorph XX121",
+    "type": "Biological",
+    "dangerLevel": "Extreme",
+    "description": "Highly aggressive endoparasitoid extraterrestrial species.",
+    "status": "active"
+ */
+
+let names;
+let type;
+let dangerLevel;
+let description;
+let status;
 
 async function ServidorConnection() {
     const response = await fetch("http://localhost:5000/entries")
         .then(response => response.json())
         .then(data => {
-            data.forEach(element => {
-                const entries = document.getElementById("entries");
-                entries.textContent = JSON.stringify(element.name);
-                console.log(element.name);
-            });
+            //const names = data.map(element => element.name);
+            // const entries = document.getElementById("entries");
+            //entries.textContent = names[0];
         })
         .catch(error => console.log(error));
 
